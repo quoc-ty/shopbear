618eb4585f9c5c89e8adb19fac44226186d5cf6e
Quoc-Ty <nguyenquocty080259@gmail.com>
2026-09-16 15:06:35 +0700

    chore: initialize Spring Boot project

---
+++ src/main/java/com/shopbear/ShopBearApplication.java
@@ -0,0 +1,13 @@
+package com.shopbear;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopBearApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShopBearApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/shopbear/ShopBearApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shopbear;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopBearApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
